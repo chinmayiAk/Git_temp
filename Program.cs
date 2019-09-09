fa292707edb76a68f0c60259a873125a57158c7d
Karmalkar <AACAK@pgs.net>
2019-09-09 19:07:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
